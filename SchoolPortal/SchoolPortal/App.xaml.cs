9650478cb684b2c8aa61ff397257dded06ff1645
dmitriy451 <76558463+dmitriy451@users.noreply.github.com>
2022-02-27 20:51:13 +0300

    Commit1

---
+++ SchoolPortal/SchoolPortal/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SchoolPortal
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml и данная программа сделана Шкуренковым
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
